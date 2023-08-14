a68c8a8d722f5889535906f81530d09fdae228c0
Kristian Paul Dizon Enore <krisore.dev@outlook.ph>
2023-08-14 17:48:37 +0800

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,19 @@
+using Khronica.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+builder.Services.AddHttpClient("Khronica.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Khronica.ServerAPI"));
+
+await builder.Build().RunAsync();
